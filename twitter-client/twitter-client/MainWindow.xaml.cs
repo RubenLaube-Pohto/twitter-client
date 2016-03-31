f30b59c125d9bb360cddecf8cf85408287e9225c
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-31 13:27:52 +0300

    Get basic example working

--- twitter-client/twitter-client/MainWindow.xaml.cs
+++ twitter-client/twitter-client/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,30 @@ namespace twitter_client
         public MainWindow()
         {
             InitializeComponent();
+            GetConnection();
+        }
+
+        private void GetConnection()
+        {
+            string consumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
+            string consumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
+            // Pass your credentials to the service
+            TwitterService service = new TwitterService(consumerKey, consumerSecret);
+
+            // Step 1 - Retrieve an OAuth Request Token
+            OAuthRequestToken requestToken = service.GetRequestToken();
+
+            // Step 2 - Redirect to the OAuth Authorization URL
+            Uri uri = service.GetAuthorizationUri(requestToken);
+            Process.Start(uri.ToString());
+
+            // Step 3 - Exchange the Request Token for an Access Token
+            string verifier = "123456"; // <-- This is input into your application by your user
+            OAuthAccessToken access = service.GetAccessToken(requestToken, verifier);
+
+            // Step 4 - User authenticates using the Access Token
+            service.AuthenticateWith(access.Token, access.TokenSecret);
+            //IEnumerable<TwitterStatus> mentions = service.ListTweetsMentioningMe(ListTweetsMentioningMeOptions.Equals(;
         }
     }
 }
